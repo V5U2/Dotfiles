67c20aa86590fabe8288b56e8c3b5c0839a3e92a
ooimo <jlborrett@gmail.com>
2014-09-25 10:16:27 +0800

    Add vim, dwb, zsh and nitrogen. Create update script.

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp -r ~/.i3 ./
+cp -r ~/.vim ./
+cp -r ~/.config/dwb ./.config/dwb
+cp -r ~/.config/zathura ./.config/zathura
+cp -r ~/.config/nitrogen ./.config/nitrogen
+cp -r ~/.config/ranger ./.config/ranger
+
+cp ~/.Xdefaults ./
+cp ~/.vimrc ./
+cp ~/.viminfo ./
+cp ~/.zshrc ./
+cp ~/.zshalias ./
